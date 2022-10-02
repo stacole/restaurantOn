2e9fca18708f512d6ada18591cb4d1b6324a227e
stacole <jmcagogosarul@gmail.com>
2022-10-02 09:19:28 -0500

    Registro de APP Vendor

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
+
+from vendor.forms import VendorForm
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
 
 # Create your views here.
@@ -37,3 +39,38 @@ def registerUser(request):
         'form': form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+def registerVendor(request):
+    if request.method == 'POST':
+        # Store the data and create the user
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid:
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name,username=username, email=email, password=password) 
+            user.role = User.VENDOR #Vendor viene de accounts.models
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user) # from accounts.signals
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Your account has been registered suessfully! Please wait for the approval.')
+            return redirect('registerVendor')
+        else:
+            print('Invalid form')
+            print(form.errors)
+
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+
+    context = {
+        'form': form,
+        'v_form': v_form,
+    }
+    return render(request,'accounts/registerVendor.html', context)
