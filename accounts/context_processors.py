8e3f1f7d90810b47deaef9918e6161c81321d70c
stacole <jmcagogosarul@gmail.com>
2022-10-09 10:42:41 -0500

    restaurant profile y geolocalizacion de goorle appi

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,4 +1,5 @@
 from vendor.models import Vendor
+from django.conf import settings
 
 def get_vendor(request):
     try:
@@ -7,3 +8,6 @@ def get_vendor(request):
         # Esto es por si no hay un usuario logeado no de error.
         vendor = None
     return dict(vendor=vendor)
+
+def get_google_api(request):
+    return{'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
