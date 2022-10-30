f4bc2c0d7f9fa5327011ae5e21a47e5865f31baf
stacole <jmcagogosarul@gmail.com>
2022-10-29 19:56:22 -0500

    get user's current location and organize the homepage to show the nearby restaurants

--- restaurantOn_main/views.py
+++ restaurantOn_main/views.py
@@ -4,9 +4,37 @@ from django.http import HttpResponse
 
 from vendor.models import Vendor
 
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
+from django.contrib.gis.db.models.functions import Distance
+
+def get_or_set_current_location(request):
+    if 'lat' in request.session:
+        lat = request.session['lat']
+        lng = request.session['lng']
+        return lng, lat
+
+    elif 'lat' in request.GET:
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
+        request.session['lat'] = lat
+        request.session['lng'] = lng
+        return lng, lat
+
+    else:
+        return None
+
 def home(request):
-    # [:4] es para solo m ostrar 4 restaurantes en linea al inicio
-    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:4] # mostrar restaurantes al inicio si son aprovados.
+    if get_or_set_current_location(request)is not None:
+        pnt = GEOSGeometry('POINT(%s %s)' % (get_or_set_current_location(request)))
+        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+
+        for v in vendors:
+            v.kms = round(v.distance.km, 1) # en vendors si se cambia distance, cambiar aqui tambien.
+
+    else:
+        # [:4] es para solo m ostrar 4 restaurantes en linea al inicio
+        vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:4] # mostrar restaurantes al inicio si son aprovados.
     #print(vendors)  imprime los restaurantes en la consola
     context = {
         'vendors': vendors,
